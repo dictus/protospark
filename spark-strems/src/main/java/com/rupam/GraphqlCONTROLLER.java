638dc8530631f192b2ab7342a1d172bb27457b12
rupambhattacharya <kolrupam0077@gmail.com>
2023-08-21 20:00:36 +0530

    spark setup with java 1.8

--- spark-strems/src/main/java/com/rupam/GraphqlCONTROLLER.java
+++ spark-strems/src/main/java/com/rupam/GraphqlCONTROLLER.java
@@ -1,16 +1,13 @@
 package com.rupam;
 
-import com.google.protobuf.InvalidProtocolBufferException;
-import com.google.protobuf.util.JsonFormat;
-import com.rupam.dto.Person;
 import com.rupam.service.SparkJob;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
 
@@ -40,14 +37,13 @@ public class GraphqlCONTROLLER {
     @Autowired
     private SparkJob sparkJob;
     @GetMapping("/count-words")
-    public Mono<Long> countWords() {
+    public ResponseEntity<Long> countWords() {
         JavaRDD<String> sentences = sparkContext.parallelize(Arrays.asList(
                 "Hello world",
                 "Spring WebFlux and Spark integration",
                 "Reactive programming"
         ));
 
-        long wordCount = sparkJob.countWordsInSentences(sparkContext, sentences);
-        return Mono.just(wordCount);
+        return new ResponseEntity<>( sparkJob.countWordsInSentences(sparkContext, sentences), HttpStatus.ACCEPTED);
     }
 }
